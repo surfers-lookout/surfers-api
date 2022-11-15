bfb733ea817c41707904d017564bb6192ca31571
davlloyd <davidlloyd0@gmail.com>
2022-11-15 15:07:10 +0800

    update namespace

--- techdocs_publish.sh
+++ techdocs_publish.sh
@@ -3,5 +3,5 @@
 # Script to generate and publish techdopcs from current directory to gcs
 export GOOGLE_APPLICATION_CREDENTIALS=~/gcr.json
 techdocs-cli generate --source-dir . 
-techdocs-cli publish --publisher-type googleGcs --storage-name techdocs-tap --entity alpha/Component/surfersapi
+techdocs-cli publish --publisher-type googleGcs --storage-name techdocs-tap --entity default/Component/surfersapi
 
